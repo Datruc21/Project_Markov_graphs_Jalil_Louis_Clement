667d576e3a6989aa5cf64fa22bc1881c1785c033
jalil <jalil.bellahcen@gmail.com>
2025-10-20 14:56:31 +0200

    Structures created and associated functions

--- utils.c
+++ utils.c
@@ -27,3 +27,58 @@ static char *getID(int i)
 
     return buffer;
 }
+
+p_cell createCell(int arrivalVertex, float probability) {
+    p_cell cell = malloc(sizeof(t_cell));
+    cell->arrivalVertex = arrivalVertex;
+    cell->probability = probability;
+    cell->next = NULL;
+    return cell;
+}
+
+t_list* createEmptyList() {
+    t_list *list = malloc(sizeof(t_list));
+    list->head = NULL;
+    return list;
+}
+
+void addCell(t_list* list, t_cell* cell) {
+    if (list->head == NULL) {
+        list->head = cell;
+    }
+    else {
+        t_cell* temp = list->head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = cell;
+    }
+}
+
+void displayList(t_list* list) {
+    printf("[head @]");
+    t_cell* temp = list->head;
+    while (temp->next != NULL) {
+        printf("->");
+        printf("(%d, %f)",temp->arrivalVertex,temp->probability);
+        temp = temp->next;
+    }
+}
+
+t_adjacency_list* createAdjacencyList(int size) {
+    t_adjacency_list* adjacency_list = malloc(sizeof(t_adjacency_list));
+    adjacency_list->size = size;
+    adjacency_list->lists = malloc(sizeof(t_list*) * size);
+    return adjacency_list;
+}
+
+void displayAdjacencyList(t_adjacency_list* adjacency_list) {
+    for (int i = 0; i < adjacency_list->size; i++) {
+        printf("List for vertex %d : ",i+1);
+        t_list* line = adjacency_list->lists[i];
+        displayList(line);
+        printf("\n");
+    }
+}
+
+
--- utils.h
+++ utils.h
@@ -7,6 +7,7 @@ struct s_cell{
     struct s_cell *next;
 };
 typedef struct s_cell t_cell;
+typedef t_cell* p_cell;
 
 struct s_list {
     t_cell* head;
@@ -15,9 +16,16 @@ typedef struct s_list t_list;
 
 struct s_adjacency_list {
     int size;
-    t_list* head;
+    t_list** lists;
 };
+typedef struct s_adjacency_list t_adjacency_list;
 
+p_cell createCell(int arrivalVertex, float probability);
+t_list* createEmptyList();
+void addCell(t_list* list, t_cell* cell);
+void displayList(t_list* list);
+t_adjacency_list* createAdjacencyList(int size);
+void displayAdjacencyList(t_adjacency_list* adjacency_list);
 
 
 #endif
