8cfc7e4e8e5dc71c91cb4a895aacd9d71a592fcb
jalil <jalil.bellahcen@gmail.com>
2025-10-20 14:24:30 +0200

    Structures created

--- utils.h
+++ utils.h
@@ -1,5 +1,23 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
+struct s_cell{
+    int arrivalVertex;
+    float probability;
+    struct s_cell *next;
+};
+typedef struct s_cell t_cell;
+
+struct s_list {
+    t_cell* head;
+};
+typedef struct s_list t_list;
+
+struct s_adjacency_list {
+    int size;
+    t_list* head;
+};
+
+
 
 #endif
