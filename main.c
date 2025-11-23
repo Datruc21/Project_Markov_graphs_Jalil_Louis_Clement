5d0f022948639d9c4f30770746deba245c0189ff
Louis Mz <lousi2606@gmail.com>
2025-11-23 16:26:04 +0100

    Adding comments in main.c

--- main.c
+++ main.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "utils.h"
-#include "matrix.h"
 
 int main() {
     // Step 1 : Create a graph
     t_adjacency_list* graph = readGraph("../data/exemple1.txt");
-
     displayAdjacencyList(graph);
     printf("\n");
-    float** matrix = createMatrix(graph);
-    displayMatrix(matrix, graph->size);
-    freeMatrix(matrix, graph->size);
-    float** A = matrixMultiplication(matrix,matrix,graph->size);
-    A = matrixMultiplication(A,matrix,graph->size);
-    displayMatrix(A, graph->size);
-    for (int i = 0; i < 4; i++) {
-        A = matrixMultiplication(matrix,matrix,graph->size);
-    }
-    displayMatrix(A, graph->size);
 
     // Step 2 : Check the graph
     printf("--- Validation of step 2 ---\n");
@@ -29,6 +16,23 @@ int main() {
     printf("--- Validation of step 3 ---\n");
     representationGraph(graph);
     printf("Result in ../data/Output.txt\n");
+
+    // Just some experiments
+    /*
+    printf("test1\n");
+    float** matrix = createMatrix(graph);
+    displayMatrix(matrix, graph->size);
+    freeMatrix(matrix, graph->size);
+
+    printf("\ntest2\n");
+    float** A = matrixMultiplication(matrix,matrix,graph->size);
+    A = matrixMultiplication(A,matrix,graph->size);
+    displayMatrix(A, graph->size);
+    for (int i = 0; i < 4; i++) {
+        A = matrixMultiplication(matrix,matrix,graph->size);
+    }
+    displayMatrix(A, graph->size);
+    */
 
     return 0;
 }
